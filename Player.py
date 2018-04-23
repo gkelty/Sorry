178fcec1852792c3ca791d4b213ccb349f2090e4
yzhang45 <yzhang45@uvm.edu>
2018-04-23 18:30:03 -0400

    player class

---
+++ Player.py
@@ -0,0 +1,30 @@
+import pygame
+import random
+
+
+color = ["red","yellow","Green","Blue"]
+
+
+class Player:
+
+     def _init_(self, color, mean, smart,human):
+
+         self.color = color
+         self.mean = mean
+         self.smart = smart
+         self.human = human
+
+     def getColor(self):
+         return self.color
+
+     def getMean(self):
+         return self.mean
+
+     def getSmart(self):
+         return self.smart
+
+     def getHuman(self):
+         return self.human
+
+
+
